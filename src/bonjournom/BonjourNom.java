94b09d417cc6beff401f2f88dc6bee679d8ac0b2
Administrator <Administrator@DIGI-LAPTOP.digimode.local>
2023-05-05 08:44:25 -0400

    Initial commit

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjournom;
+
+import aleatoire.Person;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class BonjourNom {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour " + args[0] + " " + args[1] + "!" );
+        
+        
+    }
+    
+}
